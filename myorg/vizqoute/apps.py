a5c6c099a2a4ac9fcd990c22a24cc7dcf5dcc198
Delight Mapeza <delight.aegis79@gmail.com>
2026-04-26 21:15:17 +0530

    final working local setup

--- myorg/vizqoute/apps.py
+++ myorg/vizqoute/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class VizquoteQuotesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'vizquote_quotes'
+    name = 'vizqoute'
     verbose_name = "VizQuote Quotes Module"
